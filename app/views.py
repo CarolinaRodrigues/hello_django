1cce889025b13bc541cbb0c9a846f72b2768a747
CarolinaRodrigues <krodrigies@gmail.com>
2023-01-10 23:04:26 -0400

    add projeto python Django hello World

--- app/views.py
+++ app/views.py
@@ -6,6 +6,11 @@ from django.db.models import Sum, Min
 def hello(request, nome, idade):
     return HttpResponse('<h1>Hello {} de {} anos</h1>'.format(nome,idade))
 
+#Exercício para práticar
+#No mesmo projeto que criamos acrescentar uma rota que aceite na URL mais dois parâmetros do tipo inteiro e retorne a soma deste dois parâmetros.
+#A URL deverá ficar da seguinte forma:/soma/10/20
+#Com o exemplo acima, a aplicação devará retorna a soma de 10 e 20.
+#Criar rotas também para multiplicação, divisão e subtração.
 def soma(request, numero1, numero2):
     somar = numero1 + numero2
     return HttpResponse('A soma de {} e {} é: {} '.format(numero1, numero2,somar))
